97a831ac35dd65b691c3a944bb288ba759eef92b
Animesh1508 <kharabeguru@gmail.com>
2025-01-30 00:27:49 +0530

    msg

--- src/BitManipulation/HammingWeight.java
+++ src/BitManipulation/HammingWeight.java
@@ -3,18 +3,43 @@ package BitManipulation;
 public class HammingWeight {
 
 	public static void main(String[] args) {
-		System.out.println(hammingWeight(0));
+		System.out.println(hammingWeight(413));
 
 	}
 	
-	 public static int hammingWeight(int n) {
-	        int count = 0;
-	        while(n > 0)
-	        {
-	            if(n%2 == 1)
-	                count++;
-	            n= n/2;
-	        }
-	    return count;
-	    }
+//	//Approach with dividing number by 2
+//	 public static int hammingWeight(int n) {
+//	        int count = 0;
+//	        while(n > 0)
+//	        {
+//	            if(n%2 == 1)
+//	                count++;
+//	            n= n/2;
+//	        }
+//	    return count;
+//	    }
+	 
+//	//Approach with un set the rightmost set bit by doing and operation with number-1
+//	 public static int hammingWeight(int n) {
+//	 int count=0;
+//	    while(n>0){
+//	        n= n & (n-1);
+//	        count++;
+//	    }
+//	    return count;
+//	    }
+	 
+	 
+	//Approach with right shift the set bits and increase the count
+		 public static int hammingWeight(int n) {
+		 int count=0;
+		    for(int i=1; i<=32 ; i++) {
+		    	if((n>>>i & 1) == 1){
+		    		count++;
+		    	}
+		    }
+		    return count;
+		    }
+	 
+	 
 }
